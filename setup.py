147540f96b2f32481b9506c37d1e24014e07a234
Robert Poll <rob@robertpoll.com>
2019-11-28 21:54:50 +0000

    V1.0.0

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 # python setup.py sdist
-# twine upload dist/lineus-0.1.3.tar.gz
+# twine upload dist/*
 
 setup(
     name='lineus',
-    version='0.1.18',
+    version='1.0.0',
     packages=['lineus'],
-    url='https://www.line-us.com',
+    url='https://github.com/Line-us/LineUsPythonModule',
     license='MIT',
     author='Robert Poll',
     author_email='rob@line-us.com',
@@ -16,5 +16,7 @@ setup(
         'zeroconf>=0.21.3',
         'netifaces>=0.10.9',
         'ipaddress>=1.0.22',
-    ]
+    ],
+    keywords='Line-us lineus drawing robot',
+
 )
